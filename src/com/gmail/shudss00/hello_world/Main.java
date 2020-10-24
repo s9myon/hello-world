0f54a534c7243ff78eeaac2e3a35838e260964b4
Semyon <shudss00@gmail.com>
2020-10-24 21:26:25 +0400

    Написал программу "Hello World" на Java

---
+++ src/com/gmail/shudss00/hello_world/Main.java
@@ -0,0 +1,10 @@
+package com.gmail.shudss00.hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
